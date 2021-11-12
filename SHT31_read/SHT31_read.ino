31bc75883f78b5e60ab8a13d2bf98d03753a94b6
Klaas Padeken <klaas.padeken@cern.ch>
2021-11-12 12:30:54 +0100

    initial commit

---
+++ SHT31_read/SHT31_read.ino
@@ -0,0 +1,81 @@
+/*************************************************** 
+  This is an example for the SHT31-D Humidity & Temp Sensor
+
+  Designed specifically to work with the SHT31-D sensor from Adafruit
+  ----> https://www.adafruit.com/products/2857
+
+  These sensors use I2C to communicate, 2 pins are required to  
+  interface
+ ****************************************************/
+ 
+#include <Arduino.h>
+#include <Wire.h>
+#include "Adafruit_SHT31.h"
+
+bool enableHeater = false;
+uint8_t loopCnt = 0;
+const int heaterPin =  2;
+
+Adafruit_SHT31 sht31 = Adafruit_SHT31();
+
+void setup() {
+  Serial.begin(9600);
+
+  while (!Serial)
+    delay(10);     // will pause Zero, Leonardo, etc until serial console opens
+
+  Serial.println("SHT31 test");
+//  if (! sht31.begin(0x44)) {   // Set to 0x45 for alternate i2c addr //connected low (gnd)
+  if (! sht31.begin(0x45)) {   // Set to 0x45 for alternate i2c addr //connected high (VDD)
+    Serial.println("Couldn't find SHT31");
+    while (1) delay(1);
+  }
+  sht31.heater(enableHeater);
+
+  Serial.print("Heater Enabled State: ");
+  if (sht31.isHeaterEnabled())
+    Serial.println("ENABLED");
+  else
+    Serial.println("DISABLED");
+  pinMode(heaterPin, OUTPUT);
+}
+
+
+void loop() {
+  float t = sht31.readTemperature();
+  float h   = sht31.readHumidity();
+
+  if (! isnan(t)) {  // check if 'is not a number'
+    Serial.print("Temp *C = "); Serial.println(t); //Serial.print("\t\t");
+  } else { 
+    Serial.println("Failed to read temperature");
+  }
+  
+  if (! isnan(h)) {  // check if 'is not a number'
+    Serial.print("Hum. % = "); Serial.println(h);
+  } else { 
+    Serial.println("Failed to read humidity");
+   }
+
+  delay(1000);
+//  if(t<22.){
+//    digitalWrite(heaterPin, HIGH);
+//  }
+//  else{
+//    digitalWrite(heaterPin, LOW);
+//  }
+
+  // Toggle heater enabled state every 30 seconds
+  // An ~3.0 degC temperature increase can be noted when heater is enabled
+//  if (++loopCnt == 30) {
+//    enableHeater = !enableHeater;
+//    sht31.heater(enableHeater);
+//    Serial.print("Heater Enabled State: ");
+//    if (sht31.isHeaterEnabled())
+//      Serial.println("ENABLED");
+//    else
+//      Serial.println("DISABLED");
+//
+//    loopCnt = 0;
+//  }
+}
---
+++ doc/pcb_connections.md
@@ -0,0 +1,10 @@
+Temperature Probe
+
+![pcb-brd.png](pic/b3de54f2d79d4e1fb337dd0ce793dbc1.png)
+
+SDA -> A4 D18
+SCL -> A5 D19
+
+VDD 5V
+GND GND
+
